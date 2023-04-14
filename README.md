662082821da10bb403b2fef309c588fab064328d
aeiea <118844761+aeiea@users.noreply.github.com>
2023-04-14 16:24:08 -0700

    hi

--- README.md
+++ README.md
@@ -14,6 +14,48 @@ flowchart
     g -->|Hey, I need this file!| e{Is file a valid file?} -->|Yes|h(Return the file) --> g
     e -->|No| ire
 ```
+### What are the supported file types?
+Quoting form app.py:
+```python
+sft = [
+    # text
+    'text/html',
+    'text/plain',
+    'text/css',
+    'text/javascript',
+    'text/markdown',
+    # image
+    'image/png',
+    'image/jpeg',
+    'image/gif',
+    'image/svg+xml',
+    'image/webp',
+    'image/x-icon',
+    # audio
+    'audio/mpeg',
+    'audio/ogg',
+    'audio/wav',
+    'audio/webm',
+    # video
+    'video/mp4',
+    'video/ogg',
+    'video/webm',
+    # application
+    'application/javascript',
+    'application/json',
+    'application/pdf',
+    'application/xml',
+    'application/zip',    
+]
+```
+### What about other file types?
+> Notice: `mkv` will not be supported, as it is a container format, not a file format.
+To add support for other file types, you can add them to the `sft` list in `app.py`, then write a parser.
+After that, you can add the parser to the `parsers` dictionary in `app.py`.
+Example entry:
+```python
+'application/zip': parse_zip
+```
 ## How do I contribute?
 `open a pull request, for god's sake`
 If you make enough contributions, you might become a posh collaborator!
---
+++ app.py
@@ -0,0 +1,93 @@
+'''
+NOOTSUITE
+quick and dirty privacy suite powered by python + flask, deployable in a single touch
+
+> Notice: `mkv` will not be supported, as it is a container format, not a file format.
+To add support for other file types, you can add them to the `sft` list in `app.py`, then write a parser.
+After that, you can add the parser to the `parsers` dictionary in `app.py`.
+Example entry:
+```python
+'application/zip': parse_zip
+```
+'''
+home_template = '''
+<!DOCUTYPE html>
+<html>
+    <body>
+        <h1>NOOTSUITE</h1>
+        <p>quick and dirty privacy suite powered by python + flask, deployable in a single touch</p>
+        <input type="text" id="url" placeholder="Enter URL here">
+        <button onclick="window.location.href = '/'+encodeURIComponent(document.getElementById('url').value)">Get</button>
+    </body>
+</html>
+'''
+from flask import *
+from bleach import clean
+import urllib.parse
+import requests
+import bs4
+
+# Supported file types
+sft = [
+    # text
+    'text/html',
+    'text/plain',
+    'text/css',
+    'text/javascript',
+    'text/markdown',
+    # image
+    'image/png',
+    'image/jpeg',
+    'image/gif',
+    'image/svg+xml',
+    'image/webp',
+    'image/x-icon',
+    # audio
+    'audio/mpeg',
+    'audio/ogg',
+    'audio/wav',
+    'audio/webm',
+    # video
+    'video/mp4',
+    'video/ogg',
+    'video/webm',
+    # application
+    'application/javascript',
+    'application/json',
+    'application/pdf',
+    'application/xml',
+    'application/zip',    
+]
+
+# Misc Functions
+def get_content_type(url): # Makes a HEAD request to the URL and returns the content type
+    return requests.head(url).headers['content-type']
+
+# Parsers
+# text parsers
+def parse_html(url):
+    return str(bs4.BeautifulSoup(requests.get(url).text, 'html.parser'))
+def parse_markdown(url):
+    return str(bs4.BeautifulSoup(requests.get(url).text, 'markdown'))
+def parse_text(url):
+    return requests.get(url).text
+def parse_css(url):
+    return requests.get(url).text
+def parse_js(url):
+    return requests.get(url).text
+
+
+
+# App
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return home_template
+
+@app.route('/<path:url>')
+def get(url):
+    url = urllib.parse.unquote(url)
+    if not (url.startswith('http://') or url.startswith('https://')):
+        url = 'http://' + url # default to http
+    return clean(requests.get(url))
+app.run(port = 80)
